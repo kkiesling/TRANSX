4c4e5aa5b36e3d5d8d468a82f5e6f0c8ce00cb06
Kalin Kiesling <krkiesling@gmail.com>
2016-09-09 14:31:54 -0500

    optional card 2 parameters

--- make_transx_input.py
+++ make_transx_input.py
@@ -1,6 +1,6 @@
 # download data from Fendl 3.1b
 import os
-
+import argparse as ap
 
 def download_data(data_path):
 	print('Downloading Fendl 3.1b data to ./data/')
@@ -13,8 +13,20 @@ def card1():
 	c = "Fendl 3.1b Neutron Data\n"
 	return(c)
 
-def card2():
-	pass
+def card2(**kwargs):
+	
+	if 'IPRINT' in kwargs:
+		IPRINT = kwargs['IPRINT']
+	else:
+		IPRINT = 0
+		print("Set Card 2 IPRINT to default value 0 (long)")
+	
+	if 'IOUT' in kwargs:
+		IOUT = kwargs['IOUT']
+	else:
+		IOUT = 0
+		print("Set Card 2 IOUT to default value 0 (fido)")
+
 	
 def card3():
 	pass
@@ -45,6 +57,23 @@ def card11():
 	
 def main():
 	### NEED TO MAKE THE OPTION TO CHOOSE NEUTRON, GAMMA, OR BOTH
+	
+	### PARSE COMMAND LINE ARGUMENTS ###
+	# -p = photons
+	# -n = neutrons
+	# -iprint, -iout, iprob, iset, iform, itime, idecay, itrc, icoll, initf
+	parser = ag.ArgumentParser(description='Creates TRANSX input file')
+	parser.add_argument('-n', nargs=0, default=False, type=bool)
+	parser.add_argument('-p', nargs=0, default=False, type=bool)
+	parser.add_argument('-iprint', nargs='?', default=0, type=int)
+	parser.add_argument('-iout', nargs='?', default=3, type=int)
+	parser.add_argument('-iprob', nargs='?', default=0, type=int)
+	#parser.add_argument('-iset', nargs='1', default=0, type=int)
+	parser.add_argument('-iform', nargs='?', default=1, type=int)
+	parser.add_argument('-itime', nargs='?', default=1, type=int)
+	parser.add_argument('-idecay', nargs='?', default=0, type=int)
+	parser.add_argument('-itrc', nargs='?', default=2, type=int)
+	parser.add_argument('-icoll', nargs='?', default=0, type=int)
 	
 	### DOWNLOAD & EXTRACT DATA ###
 	data_path = './data/'
