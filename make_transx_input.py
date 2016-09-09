44a0b2cb9c478b69df707755c114cd51858f2274
Kalin Kiesling <krkiesling@gmail.com>
2016-09-09 09:32:45 -0500

    skeleton functions to create each card input

--- make_transx_input.py
+++ make_transx_input.py
@@ -9,9 +9,44 @@ def download_data(data_path):
 	os.system("unzip fendl31b-neutron-matxs.zip -d {}".format(data_path))
 	os.system("rm fendl31b-neutron-matxs.zip")
 	
+def card1():
+	c = "Fendl 3.1b Neutron Data\n"
+	return(c)
+
+def card2():
+	pass
+	
+def card3():
+	pass
+	
+def card4():
+	pass
+	
+def card5():
+	pass
+
+def card6():
+	pass
+	
+def card7():
+	pass
+	
+def card8():
+	pass
+
+def card9():
+	pass
+
+def card10():
+	pass
+	
+def card11():
+	pass
 	
 def main():
-	### DOWNLOAD DATA ###
+	### NEED TO MAKE THE OPTION TO CHOOSE NEUTRON, GAMMA, OR BOTH
+	
+	### DOWNLOAD & EXTRACT DATA ###
 	data_path = './data/'
 	
 	# check if data folder already exist
@@ -28,6 +63,18 @@ def main():
 		# get data
 		download_data(data_path)
 		
+	
+	### CREATE INPUT ###  
+	# create a string for each card individually
+	
+	# card 1 - Title
+	c1 = card1()
+	
+	# card 2 - Options
+	## THESE ARE ALL OPTIONAL! so make optional card arguments?
+	
+	# card 3 - Parameters
+	# This has to be done after the rest maybe?
 
 if __name__ == "__main__":
 	main()
