6328280fcf3f20200bf4756889b87444ca4cf1f8
Kalin Kiesling <krkiesling@gmail.com>
2016-09-12 12:20:03 -0500

    card 3 partially written

--- make_transx_input.py
+++ make_transx_input.py
@@ -1,6 +1,7 @@
 # download data from Fendl 3.1b
 import os
 import argparse as ap
+import glob
 
 def download_data(data_path):
 	print('Downloading Fendl 3.1b data to ./data/')
@@ -28,8 +29,44 @@ def card2(**kwargs):
 		print("Set Card 2 IOUT to default value 0 (fido)")
 
 	
-def card3():
-	pass
+def card3(data_path):
+	# NGROUP = ??? (-217 in others)
+	NGROUP = -217
+	
+	# NL = ??
+	NL = 6
+	
+	# NTABL = ??
+	NTABL = 227
+	
+	# NUP = ??
+	NUP = 0
+	
+	# NTHG = ??
+	NTHG =0
+	
+	# NMIX = number of mixes (ie number of *.m files)
+	NMIX = len(glob.glob1(data_path, "*.m"))
+	
+	# NREG - number of regions, assume 1
+	NREG = 1
+	
+	# NMIXS = number of mix specifications
+	NMIXS = NMIX + 0 # IDK what makes this > NMIX
+	
+	# NED = number of extra edits, always assume 7 for heat, kerma, dame, dpa,
+	# t, he, and h
+	NED = 7
+	
+	# NEDS = ????
+	NEDS = NED + 0
+	
+	c3 = "{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}\n".format(NGROUP, NL,
+																 NTABL, NUP,
+																 NTHG, NMIX,
+																 NREG, NMIXS,
+																 NED, NEDS)
+	return c3
 	
 def card4():
 	pass
@@ -61,19 +98,19 @@ def main():
 	### PARSE COMMAND LINE ARGUMENTS ###
 	# -p = photons
 	# -n = neutrons
-	# -iprint, -iout, iprob, iset, iform, itime, idecay, itrc, icoll, initf
-	parser = ag.ArgumentParser(description='Creates TRANSX input file')
-	parser.add_argument('-n', nargs=0, default=False, type=bool)
-	parser.add_argument('-p', nargs=0, default=False, type=bool)
-	parser.add_argument('-iprint', nargs='?', default=0, type=int)
-	parser.add_argument('-iout', nargs='?', default=3, type=int)
-	parser.add_argument('-iprob', nargs='?', default=0, type=int)
-	#parser.add_argument('-iset', nargs='1', default=0, type=int)
-	parser.add_argument('-iform', nargs='?', default=1, type=int)
-	parser.add_argument('-itime', nargs='?', default=1, type=int)
-	parser.add_argument('-idecay', nargs='?', default=0, type=int)
-	parser.add_argument('-itrc', nargs='?', default=2, type=int)
-	parser.add_argument('-icoll', nargs='?', default=0, type=int)
+	# -iprint, -iout, iprob, iset, iform, itime, idecay, itrc, icoll, initf 
+	# parser = ag.ArgumentParser(description='Creates TRANSX input file')
+	# parser.add_argument('-n', nargs=0, default=False, type=bool)
+	# parser.add_argument('-p', nargs=0, default=False, type=bool)
+	# parser.add_argument('-iprint', nargs='?', default=0, type=int)
+	# parser.add_argument('-iout', nargs='?', default=3, type=int)
+	# parser.add_argument('-iprob', nargs='?', default=0, type=int)
+	# #parser.add_argument('-iset', nargs='1', default=0, type=int)
+	# parser.add_argument('-iform', nargs='?', default=1, type=int)
+	# parser.add_argument('-itime', nargs='?', default=1, type=int)
+	# parser.add_argument('-idecay', nargs='?', default=0, type=int)
+	# parser.add_argument('-itrc', nargs='?', default=2, type=int)
+	# parser.add_argument('-icoll', nargs='?', default=0, type=int)
 	
 	### DOWNLOAD & EXTRACT DATA ###
 	data_path = './data/'
@@ -100,10 +137,15 @@ def main():
 	c1 = card1()
 	
 	# card 2 - Options
-	## THESE ARE ALL OPTIONAL! so make optional card arguments?
+	## make optional card arguments, otherwise defaults?
 	
 	# card 3 - Parameters
 	# This has to be done after the rest maybe?
+	c3 = card3(data_path)
+	
+	# card 4 - Mix Names
+	c4 = card4(data_path)
+	
 
 if __name__ == "__main__":
 	main()
