1de29d23914103abcbd21607dce55ff8e6ff60b2
Kalin Kiesling <krkiesling@gmail.com>
2016-09-29 11:15:23 -0500

    card 2 finished, card 3 in progress

--- make_transx_input.py
+++ make_transx_input.py
@@ -14,26 +14,50 @@ def card1():
     c = "Fendl 3.1b Neutron Data\n"
     return(c)
 
-def card2():
+def card2(n_TF, g_TF):
+    # use default values
+    IPRINT = 0
+    IOUT = 3
+    IPROB = 0
 
-    #if 'IPRINT' in kwargs:
-    #   IPRINT = kwargs['IPRINT']
-    #else:
-    #   IPRINT = 0
-    #   print("Set Card 2 IPRINT to default value 0 (long)")
-    #
-    #if 'IOUT' in kwargs:
-    #   IOUT = kwargs['IOUT']
-    #else:
-    #   IOUT = 0
-    #   print("Set Card 2 IOUT to default value 0 (fido)")
+    if n_TF and g_TF:
+        ISET = 12
+    else:
+        ISET = 1
 
-    c = "idk what to do with this card yet\n"
+    IFORM = 1
+    ITIME = 1
+    IDECAY = 0
+    ITRC = 2
+    ICOLL = 0
+    INITF = 0
+
+    c = "{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}\n".format(IPRINT, IOUT, IPROB,
+                                                           ISET, IFORM, ITIME,
+                                                           IDECAY, ITRC, ICOLL,
+                                                           INITF)
+
     return c
 
-def card3(datapath):
-    # NGROUP = ??? (-217 in others)
-    NGROUP = -217
+def card3(datapath, n_TF, g_TF):
+
+    # NGROUP = neutron + gamma
+    any_file = os.listdir(datapath)[0]
+    f = open('{}/{}'.format(datapath, any_file), "r")
+    for i in range(0,8):
+        line = f.readline()
+
+    if n_TF:
+        n_group = int(line.split()[0])
+    else:
+        n_group = 0
+
+    if g_TF:
+        g_group = int(line.split()[1])
+    else:
+        g_group = 0
+
+    NGROUP = n_group + g_group
 
     # NL = ??
     NL = 6
@@ -45,7 +69,7 @@ def card3(datapath):
     NUP = 0
 
     # NTHG = ??
-    NTHG =0
+    NTHG = 0
 
     # NMIX = number of mixes (ie number of *.m files)
     NMIX = len(glob.glob1(datapath, "*.m"))
@@ -54,14 +78,14 @@ def card3(datapath):
     NREG = 1
 
     # NMIXS = number of mix specifications
-    NMIXS = NMIX + 0 # IDK what makes this > NMIX
+    NMIXS = NMIX
 
     # NED = number of extra edits, always assume 7 for heat, kerma, dame, dpa,
     # t, he, and h
     NED = 7
 
-    # NEDS = ????
-    NEDS = NED + 0
+    # NEDS
+    NEDS = NED
 
     c = "{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}\n".format(NGROUP, NL,
                                                            NTABL, NUP,
@@ -241,23 +265,6 @@ def card11():
 def main():
     ### NEED TO MAKE THE OPTION TO CHOOSE NEUTRON, GAMMA, OR BOTH
 
-    ### PARSE COMMAND LINE ARGUMENTS ###
-    # -p = photons
-    # -n = neutrons
-    # -iprint, -iout, iprob, iset, iform, itime, idecay, itrc, icoll, initf
-    # parser = ag.ArgumentParser(description='Creates TRANSX input file')
-    # parser.add_argument('-n', nargs=0, default=False, type=bool)
-    # parser.add_argument('-p', nargs=0, default=False, type=bool)
-    # parser.add_argument('-iprint', nargs='?', default=0, type=int)
-    # parser.add_argument('-iout', nargs='?', default=3, type=int)
-    # parser.add_argument('-iprob', nargs='?', default=0, type=int)
-    # #parser.add_argument('-iset', nargs='1', default=0, type=int)
-    # parser.add_argument('-iform', nargs='?', default=1, type=int)
-    # parser.add_argument('-itime', nargs='?', default=1, type=int)
-    # parser.add_argument('-idecay', nargs='?', default=0, type=int)
-    # parser.add_argument('-itrc', nargs='?', default=2, type=int)
-    # parser.add_argument('-icoll', nargs='?', default=0, type=int)
-
     ### DOWNLOAD & EXTRACT DATA ###
     datapath = './data/'
 
@@ -288,11 +295,11 @@ def main():
 
     # card 2 - Options
     ## make optional card arguments, otherwise defaults?
-    c2 = card2()
+    c2 = card2(n_TF, g_TF)
 
     # card 3 - Parameters
     # This has to be done after the rest maybe?
-    c3, NMIX = card3(datapath)
+    c3, NMIX = card3(datapath, n_TF, g_TF)
 
     # card 4 - Mix Names
     c4, names = card4(datapath)
@@ -309,6 +316,9 @@ def main():
 
     total = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + 'stop'
     print(total)
+    input_file = open("input.txt", "w")
+    input_file.write(total)
+    input_file.close()
 
 if __name__ == "__main__":
     main()
