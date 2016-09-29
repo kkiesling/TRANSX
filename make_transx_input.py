01a9b50c36e94c0d7d9a75c185ab6ef350a5e0cd
Kalin Kiesling <krkiesling@gmail.com>
2016-09-29 17:19:07 -0500

    counts number of edits

--- make_transx_input.py
+++ make_transx_input.py
@@ -20,10 +20,11 @@ def card2(n_TF, g_TF):
     IOUT = 3
     IPROB = 0
 
-    if n_TF and g_TF:
-        ISET = 12
-    else:
-        ISET = 1
+    ISET = ""
+    if n_TF:
+        ISET += "1"
+    if g_TF:
+        ISET += "2"
 
     IFORM = 1
     ITIME = 1
@@ -39,7 +40,7 @@ def card2(n_TF, g_TF):
 
     return c
 
-def card3(datapath, n_TF, g_TF):
+def card3(datapath, n_TF, g_TF, count):
 
     # NGROUP = neutron + gamma
     any_file = os.listdir(datapath)[0]
@@ -63,8 +64,6 @@ def card3(datapath, n_TF, g_TF):
     # NL = ??
     NL = 12
 
-
-
     # NUP = ??
     NUP = 0
 
@@ -85,7 +84,7 @@ def card3(datapath, n_TF, g_TF):
     NED = 7
 
     # NEDS = equals the number of entries for the edits
-    NEDS = NED + 77
+    NEDS = count
 
     # NTABL = ??
     NTABL = abs(NGROUP) + NED + NUP
@@ -121,7 +120,7 @@ def card5():
 
 def card6():
     # no card 6 in the samples?
-    return ""
+    return "/\n"
 
 def card7(names):
     c = ""
@@ -139,6 +138,7 @@ def card8():
 
 def card9(datapath, HED, n_TF, g_TF):
     c = ""
+    count = 0
 
     for i, edit in enumerate(HED):
 
@@ -147,19 +147,24 @@ def card9(datapath, HED, n_TF, g_TF):
             # check if writing for neutron, gammas, or both
             if n_TF:
                 c += '{} heat/\n'.format(i+1)
+                count += 1
             if g_TF:
                 c += '{} gheat/\n'.format(i+1)
+                count += 1
 
         # assume all mixes are used in kerma with standard multiplier
         elif edit == 'kerma':
             # check if writing for neutron, gammas, or both
             if n_TF:
                 c += '{} kerma/\n'.format(i+1)
+                count += 1
             if g_TF:
                 c += '{} gheat/\n'.format(i+1)
+                count += 1
 
         elif edit == 'dame':
             c += '{} dame/\n'.format(i+1)
+            count += 1
 
         # assume all mixes are used in dame with standard multiplier
         elif edit == 'dpa':
@@ -167,22 +172,26 @@ def card9(datapath, HED, n_TF, g_TF):
             c += '4 dame .0129 be9/\n'
             c += '4 dame .0129 c12/\n'
             c += '4 dame .016 na23/\n'
+            count += 3
 
             #c += '4 dame .016 mgnat/\n'
             c += '4 dame .016 mg23/\n'
             c += '4 dame .016 mg24/\n'
             c += '4 dame .016 mg25/\n'
             c += '4 dame .016 mg26/\n'
+            count += 4
 
             c += '4 dame .0148 al27/\n'
             c += '4 dame .016 si28/\n'
             c += '4 dame .016 si29/\n'
             c += '4 dame .016 si30/\n'
+            count += 4
 
             #c += '4 dame .01 knat/\n'
             c += '4 dame .01 k39/\n'
             c += '4 dame .01 k40/\n'
             c += '4 dame .01 k41/\n'
+            count += 3
 
             #c += '4 dame .01 canat/\n'
             c += '4 dame .01 ca40/\n'
@@ -191,34 +200,48 @@ def card9(datapath, HED, n_TF, g_TF):
             c += '4 dame .01 ca44/\n'
             c += '4 dame .01 ca46/\n'
             c += '4 dame .01 ca48/\n'
+            count += 6
 
             c += '4 dame .01 ti46/\n'
             c += '4 dame .01 ti47/\n'
             c += '4 dame .01 ti48/\n'
             c += '4 dame .01 ti49/\n'
             c += '4 dame .01 ti50/\n'
+            count += 5
 
             #c += '4 dame .01 vnat/\n'
             c += '4 dame .01 v50/\n'
             c += '4 dame .01 v51/\n'
+            count += 2
 
             c += '4 dame .01 cr50/\n'
             c += '4 dame .01 cr52/\n'
             c += '4 dame .01 cr53/\n'
             c += '4 dame .01 cr54/\n'
+            count += 4
+
             c += '4 dame .01 mn55/\n'
+            count += 1
+
             c += '4 dame .01 fe54/\n'
             c += '4 dame .01 fe56/\n'
             c += '4 dame .01 fe57/\n'
             c += '4 dame .01 fe58/\n'
+            count += 4
+
             c += '4 dame .01 co59/\n'
+            count += 1
+
             c += '4 dame .01 ni58/\n'
             c += '4 dame .01 ni60/\n'
             c += '4 dame .01 ni61/\n'
             c += '4 dame .01 ni62/\n'
             c += '4 dame .01 ni64/\n'
+            count += 5
+
             c += '4 dame .01 cu63/\n'
             c += '4 dame .01 cu65/\n'
+            count += 2
 
             #c += '4 dame .01 zrnat/\n'
             c += '4 dame .01 zr90/\n'
@@ -226,8 +249,11 @@ def card9(datapath, HED, n_TF, g_TF):
             c += '4 dame .01 zr92/\n'
             c += '4 dame .01 zr94/\n'
             c += '4 dame .01 zr96/\n'
+            count += 5
 
             c += '4 dame .01 nb93/\n'
+            count += 1
+
             c += '4 dame .00667 mo92/\n'
             c += '4 dame .00667 mo94/\n'
             c += '4 dame .00667 mo95/\n'
@@ -235,30 +261,41 @@ def card9(datapath, HED, n_TF, g_TF):
             c += '4 dame .00667 mo97/\n'
             c += '4 dame .00667 mo98/\n'
             c += '4 dame .00667 mo100/\n'
+            count += 7
 
             c += '4 dame .00444 ta181/\n'
+            count += 1
+
             c += '4 dame .00444 w182/\n'
             c += '4 dame .00444 w183/\n'
             c += '4 dame .00444 w184/\n'
             c += '4 dame .00444 w186/\n'
+            count += 4
+
             c += '4 dame .01333 au197/\n'
             c += '4 dame .016 pb206/\n'
+            count += 2
+
             c += '4 dame .016 pb207/\n'
             c += '4 dame .016 pb208/\n'
+            count += 2
 
         elif edit == 't':
             c += '{} * .h3*/\n'.format(i+1)
+            count += 1
 
         elif edit == 'he':
             c += '{} * .he3*/\n'.format(i+1)
             c += '{} * .he4*/\n'.format(i+1)
+            count += 2
 
         elif edit == 'h':
             c += '{} * .h1*/\n'.format(i+1)
             c += '{} * .h2*/\n'.format(i+1)
             c += '{} * .h3*/\n'.format(i+1)
+            count += 3
 
-    return c
+    return c, count
 
 def card10():
     pass
@@ -289,7 +326,7 @@ def main():
     # Use neutron or gamma or both
     # set both to true for now
     n_TF = True
-    g_TF = True
+    g_TF = False
 
     ## BBC
 
@@ -303,9 +340,6 @@ def main():
     ## make optional card arguments, otherwise defaults?
     c2 = card2(n_TF, g_TF)
 
-    # card 3 - Parameters
-    # This has to be done after the rest maybe?
-    c3, NMIX = card3(datapath, n_TF, g_TF)
 
     # card 4 - Mix Names
     c4, names = card4(datapath)
@@ -318,7 +352,11 @@ def main():
 
     c8, HED = card8()
 
-    c9 = card9(datapath, HED, n_TF, g_TF)
+    c9, count = card9(datapath, HED, n_TF, g_TF)
+
+    # card 3 - Parameters
+    # This has to be done after the rest maybe?
+    c3, NMIX = card3(datapath, n_TF, g_TF, count)
 
     total = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + 'stop'
     input_file = open("input.txt", "w")
